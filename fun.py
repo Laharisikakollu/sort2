0225765803222d0e6500a3dc847d777285d185dc
Laharisikakollu <amlaharisikakollu@gmail.com>
2020-01-09 11:15:48 +0000

    Initial commit

---
+++ fun.py
@@ -0,0 +1,20 @@
+#import operator
+def freq(lis):
+  res=[]
+  freqi={}
+  for i in lis:
+    l=len(i)
+    if(l in freqi):
+      freqi[l]+=1
+    else:
+      freqi[l]=1
+  freqi={k: v for k, v in sorted(freqi.items(), key=lambda item: item[1])}
+  #print(freqi)
+  k=list(freqi.keys())
+  v=list(freqi.values())
+  for d in range(len(k)):
+    for i in lis:
+      if len(i)==k[d]:
+        res.append(i)
+        
+  print(res)
